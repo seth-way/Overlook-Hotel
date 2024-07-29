5919aec1641891f8987df69452fce2687145771e
Seth Way <way.sethaniel@gmail.com>
2024-07-29 17:23:23 -0400

    write initial tests for bookings and rooms

--- test/bookings-test.js
+++ test/bookings-test.js
@@ -1,8 +1,188 @@
 import chai from 'chai';
 const expect = chai.expect;
+import { bookings } from './data/sample-bookings';
+import {
+  getBookingsByCustomer,
+  getBookingsByDate,
+  groupBookingsPastVsUpcoming,
+} from '../src/bookings';
 
-describe('See if the bookings tests are running', function() {
-  it('should return true', function() {
-    expect(true).to.equal(true);
+describe('Get all bookings for a customer', () => {
+  const allBookings = [...bookings];
+
+  it('should return all bookings associated with a customer id', () => {
+    const expected = [
+      {
+        id: '5fwrgu4i7k55hl7bd',
+        userID: 1,
+        date: '2022/02/18',
+        roomNumber: 1,
+      },
+      {
+        id: '5fwrgu4i7k55hl7dx',
+        userID: 1,
+        date: '2022/02/21',
+        roomNumber: 14,
+      },
+    ];
+    const result = getBookingsByCustomer(1, allBookings);
+    expect(result).to.deep.equal(expected);
+  });
+
+  it('should work with any customer id', () => {
+    const expected = [
+      {
+        id: '5fwrgu4i7k55hl7dw',
+        userID: 2,
+        date: '2022/01/12',
+        roomNumber: 20,
+      },
+    ];
+    const result = getBookingsByCustomer(2, allBookings);
+    expect(result).to.deep.equal(expected);
+  });
+
+  it('should return an empty array if id is not found', () => {
+    const expected = [];
+    const result = getBookingsByCustomer(99, allBookings);
+    expect(result).to.deep.equal(expected);
+  });
+});
+
+describe('Get bookings for a given date', () => {
+  const allBookings = [...bookings];
+
+  it('should return all bookings for a given date', () => {
+    const expected = [
+      {
+        id: '5fwrgu4i7k55hl7e5',
+        userID: 6,
+        date: '2025/1/12',
+        roomNumber: 15,
+      },
+      {
+        id: '5fwrgu4i7k55hl7nt',
+        userID: 5,
+        date: '2025/01/12',
+        roomNumber: 3,
+      },
+    ];
+    const testDate = new Date('2025/01/12');
+    const result = getBookingsByDate(testDate, allBookings);
+    expect(result).to.deep.equal(expected);
+  });
+
+  it('should return all bookings for any date', () => {
+    const expected = [
+      {
+        id: '5fwrgu4i7k55hl7bd',
+        userID: 1,
+        date: '2022/02/18',
+        roomNumber: 1,
+      },
+    ];
+    const testDate = new Date('2022/02/18');
+    const result = getBookingsByDate(testDate, allBookings);
+    expect(result).to.deep.equal(expected);
+  });
+
+  it('should return an empty array of no bookings are found', () => {
+    const expected = [];
+    const testDate = new Date('1970/02/18');
+    const result = getBookingsByDate(testDate, allBookings);
+    expect(result).to.deep.equal(expected);
+  });
+});
+
+describe('Group bookings by past & upcoming', () => {
+  const allBookings = [...bookings];
+
+  it('should take bookings & sort them into past / upcoming', () => {
+    const expected = {
+      past: [
+        {
+          id: '5fwrgu4i7k55hl7bd',
+          userID: 1,
+          date: '2022/02/18',
+          roomNumber: 1,
+        },
+        {
+          id: '5fwrgu4i7k55hl7dw',
+          userID: 2,
+          date: '2022/01/12',
+          roomNumber: 20,
+        },
+        {
+          id: '5fwrgu4i7k55hl7dx',
+          userID: 1,
+          date: '2022/02/21',
+          roomNumber: 14,
+        },
+        {
+          id: '5fwrgu4i7k55hl7e4',
+          userID: 8,
+          date: '2022/01/12',
+          roomNumber: 1,
+        },
+      ],
+      upcoming: [
+        {
+          id: '5fwrgu4i7k55hl7e5',
+          userID: 6,
+          date: '2025/01/12',
+          roomNumber: 15,
+        },
+        {
+          id: '5fwrgu4i7k55hl7nt',
+          userID: 5,
+          date: '2025/01/12',
+          roomNumber: 3,
+        },
+      ],
+    };
+
+    const result = groupBookingsPastVsUpcoming(allBookings);
+    expect(result).to.deep.equal(expected);
+  });
+
+  it('should create an empty array value when no bookings found', () => {
+    const expected = {
+      past: [
+        {
+          id: '5fwrgu4i7k55hl7bd',
+          userID: 1,
+          date: '2022/02/18',
+          roomNumber: 1,
+        },
+        {
+          id: '5fwrgu4i7k55hl7dw',
+          userID: 2,
+          date: '2022/01/12',
+          roomNumber: 20,
+        },
+        {
+          id: '5fwrgu4i7k55hl7dx',
+          userID: 1,
+          date: '2022/02/21',
+          roomNumber: 14,
+        },
+        {
+          id: '5fwrgu4i7k55hl7e4',
+          userID: 8,
+          date: '2022/01/12',
+          roomNumber: 1,
+        },
+      ],
+      upcoming: [],
+    };
+
+    const result = groupBookingsPastVsUpcoming(allBookings.slice(0, 4));
+    expect(result).to.deep.equal(expected);
+  });
+
+  it('should return both empty arrays when given no bookings', () => {
+    const expected = { past: [], upcoming: [] };
+    const result = groupBookingsPastVsUpcoming([]);
+    expect(result).to.deep.equal(expected);
   });
 });
--- test/rooms-test.js
+++ test/rooms-test.js
@@ -1,8 +1,17 @@
 import chai from 'chai';
 const expect = chai.expect;
+import { rooms } from './data/sample-rooms';
+import { bookings } from './data/sample-bookings';
+import { getAvailableRooms } from '../src/rooms';
 
-describe('See if the rooms tests are running', function() {
-  it('should return true', function() {
-    expect(true).to.equal(true);
+describe('Should return a list of available rooms on a given date', () => {
+  const allRooms = [...rooms];
+  const allBookings = [...bookings];
+
+  it('return all rooms with availability on the given date', () => {
+    const expected = [];
+    const testDate = new Date('2025/01/12');
+    const result = getAvailableRooms(testDate, allRooms, allBookings);
+    expect(result).to.deep.equal(expected);
   });
 });
