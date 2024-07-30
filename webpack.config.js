ae5919c712effb14b96a3fd63b924f612a33919f
Seth Way <way.sethaniel@gmail.com>
2024-07-30 13:28:05 -0400

    add html header & styling

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'none',
-  entry: './src/index.js',
+  entry: './src/scripts.js',
   output: {
     path: __dirname + '/dist',
     filename: 'bundle.js',
